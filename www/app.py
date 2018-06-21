29c0ef969e9bfd8979e8bcc8a18d06e05ba6e1b4
ZHEN <zhen0623@outlook.com>
2018-06-21 22:33:37 +0800

    创建连接池

--- www/app.py
+++ www/app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from aiohttp import web
 
 def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>')
+    return web.Response(body='<h1>Hello Python</h1>')
 
 @asyncio.coroutine
 def init(loop):
@@ -19,3 +19,20 @@ def init(loop):
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
+
+@asyncio.coroutine
+def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global _pool
+    _pool = yield from aiomysql.create_pool(
+        host=kw.get('host','localhost'),
+        port=kw.get('port',3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset','utf8'),
+        autocommit=kw.get('autocommit',True),
+        maxsize=kw.get('maxsize',10),
+        minsize=kw.get('minsize',1),
+        loop=loop
+    )
